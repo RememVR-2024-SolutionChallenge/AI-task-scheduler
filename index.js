eecb5aa85b7a415eec7122d6d62b848881c6bc56
woog2roid <woog2roid@gmail.com>
2024-02-01 23:15:29 +0900

    fix: Switch the logic order and add CORS.

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const functions = require("@google-cloud/functions-framework");
 const axios = require("axios");
 const redis = require("redis");
+const { send } = require("./src/utils");
 
 require("dotenv").config();
-
-const { send } = require("./src/utils");
 
 const redisClient = redis.createClient({
   url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
 });
 
+// 0. Setup CORS && Connect to `Redis Queue`
+// 1. Check status of `AI Engine`.
+// 2. Get the next task from `Redis Queue`.
+// 3. Trigger `AI Engine` to process the next task.
+// 4. Update current task and pop `Redis Queue`
+// 5. Response to requestor
+
 functions.http("engineTrigger", async (req, res) => {
-  // 0. Connect to `Redis Queue`
+  // 0. Setup CORS && Connect to `Redis Queue`
+  res.set("Access-Control-Allow-Origin", "*");
   await redisClient.connect().catch(async (err) => {
     return await send(res, redisClient, 500, "Redis Queue: Error");
   });
@@ -32,16 +39,16 @@ functions.http("engineTrigger", async (req, res) => {
   if (taskId === undefined || taskId === null)
     return send(res, redisClient, 400, "Redis Queue: There is no next task.");
 
-  // 3. Update current task and pop `Redis Queue`
-  await redisClient.set("ai-current-task", taskId);
-  await redisClient.lPop("ai-queue");
-
-  // 4. Trigger `AI Engine` to process the next task.
+  // 3. Trigger `AI Engine` to process the next task.
   await axios
     .post(process.env.AI_ENGINE_URL, { taskId: taskId })
     .catch(async (err) => {
       return send(res, redisClient, 500, "AI Engine: Error");
     });
+
+  // 4. Update current task and pop `Redis Queue`
+  await redisClient.set("ai-current-task", taskId);
+  await redisClient.lPop("ai-queue");
 
   // 5. Response to requestor
   return send(res, redisClient, 200, "Successfully triggered.");
