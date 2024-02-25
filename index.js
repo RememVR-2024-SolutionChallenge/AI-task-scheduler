3413d61e3a125c71a2afb0b8811d44dd4c823c14
woog2roid <woog2roid@gmail.com>
2024-02-26 01:43:29 +0900

    fix: Fix async/await try-catch, Add logger

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const send = require("./src/utils");
 
 require("dotenv").config();
 
+console.log("redis client connecting...");
 const redisClient = redis.createClient({
   url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
 });
@@ -17,39 +18,54 @@ const redisClient = redis.createClient({
 // 5. Response to requestor
 
 functions.http("engineTrigger", async (req, res) => {
+  console.log("AI Engine triggering process has started.");
+
   // 0. Setup CORS && Connect to `Redis Queue`
   res.set("Access-Control-Allow-Origin", "*");
-  await redisClient.connect().catch(async (err) => {
+  try {
+    await redisClient.connect();
+  } catch (err) {
     return await send(res, redisClient, 500, "Redis Queue: Error");
-  });
+  }
 
   // 1. Check status of `AI Engine`.
-  const response = await axios
-    .get(process.env.AI_ENGINE_URL)
-    .then(async (res) => {
-      if (res.data.status !== "IDLE")
-        return await send(res, redisClient, 400, "AI Engine: WORKING");
-    })
-    .catch(async (err) => {
-      return await send(res, redisClient, 500, "AI Engine: Error");
-    });
+  try {
+    const aiEngineStatus = await axios.get(process.env.AI_ENGINE_URL);
+    if (aiEngineStatus.data.status !== "IDLE") {
+      return await send(res, redisClient, 400, "AI Engine: WORKING");
+    }
+  } catch (err) {
+    return await send(res, redisClient, 500, "AI Engine: Error (GET /)");
+  }
 
   // 2. Get the next task from `Redis Queue`.
-  const taskId = (await redisClient.lRange("ai-queue", 0, -1))[0];
-  if (taskId === undefined || taskId === null)
-    return send(res, redisClient, 400, "Redis Queue: There is no next task.");
+  let taskId;
+  try {
+    taskId = (await redisClient.lRange("ai-queue", 0, -1))[0];
+    if (taskId === undefined || taskId === null) {
+      return send(res, redisClient, 404, "Redis Queue: There is no next task.");
+    }
+  } catch (err) {
+    return await send(
+      res,
+      redisClient,
+      500,
+      "Redis Queue: Error getting next task."
+    );
+  }
 
   // 3. Trigger `AI Engine` to process the next task.
-  await axios
-    .post(process.env.AI_ENGINE_URL + `/api/train/scene/${taskId}`)
-    .catch(async (err) => {
-      return send(res, redisClient, 500, "AI Engine: Error");
-    });
+  try {
+    await axios.post(process.env.AI_ENGINE_URL + `/api/train/${taskId}`);
+  } catch (err) {
+    return send(res, redisClient, 500, "AI Engine: Error (POST /train)");
+  }
 
   // 4. Update current task and pop `Redis Queue`
   await redisClient.set("ai-current-task", taskId);
   await redisClient.lPop("ai-queue");
 
   // 5. Response to requestor
+  console.log("AI Engine Successfully Triggered.", taskId);
   return send(res, redisClient, 200, "Successfully triggered.");
 });
